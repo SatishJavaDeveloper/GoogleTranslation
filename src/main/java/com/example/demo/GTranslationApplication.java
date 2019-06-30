bbf073f186085e1c764eb429c53c8e9c387daa43
SatishJavaDeveloper <satishreddy270694@gmail.com>
2019-06-30 19:11:57 +0530

    Translation

---
+++ src/main/java/com/example/demo/GTranslationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GTranslationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GTranslationApplication.class, args);
+	}
+
+}
